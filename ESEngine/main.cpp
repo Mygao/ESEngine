4b4964ae7e2c70b112f621ad4b51d88f88ce9d30
unknown <>
2015-12-11 00:24:34 +0900

    Remove test code.

--- ESEngine/main.cpp
+++ ESEngine/main.cpp
@@ -1,14 +1,6 @@
 #include <iostream>
 
-#include "base/message_thread.h"
-#include "btBulletDynamicsCommon.h"
-
 int main(int argc, const char** argv)
 {
-	btDefaultCollisionConfiguration* collisionConf =
-		new btDefaultCollisionConfiguration();
-
-	delete collisionConf;
-
 	return 0;
 }
